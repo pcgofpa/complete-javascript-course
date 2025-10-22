f01301efcee9809965a1185b8d3c5d948c92e365
Philip Ginter <pcgofpa@gmail.com>
2025-10-22 10:19:34 -0500

    Update fund_1_script.js
    - added template literal to notes.

--- 01-Fundamentals-Part-1/starter/fund_1_script.js
+++ 01-Fundamentals-Part-1/starter/fund_1_script.js
@@ -1,10 +1,19 @@
 // Variable declarations
+/*
 let js = "Amazing";
 let fund_1_math = 40 + 8 + 23 - 10;
-let first_name = "Phil"
-let last_name = "Ginter"
-
-/*
 console.log(fund_1_math)
 console.log(first_name)
 */
+const first_name = "Phil";
+const last_name = "Ginter";
+const current_year = 2025;
+const birth_Year = 1984;
+const age = current_year-birth_Year;
+const job = "Software Engineer";
+
+// Template Literal
+let phil_template = `I'm ${first_name} ${last_name}, a ${age} year old ${job}.`
+console.log(phil_template)
+
+
