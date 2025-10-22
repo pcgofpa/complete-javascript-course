74ce89632871d1bd258540ade5f0748eab7f3fb4
Philip Ginter <pcgofpa@gmail.com>
2025-10-22 12:10:54 -0500

    Update fund_1_script.js
    -Added notes for sections completed.

--- 01-Fundamentals-Part-1/starter/fund_1_script.js
+++ 01-Fundamentals-Part-1/starter/fund_1_script.js
@@ -1,20 +1,37 @@
 // Variable declarations
-const first_name = "Phil";
-const last_name = "Ginter";
-const current_year = 2025;
-const birth_Year = 1984;
-const age = current_year-birth_Year;
-const job = "Software Engineer";
+// const first_name = "Phil";
+// const last_name = "Ginter";
+// const current_year = 2025;
+// const birth_Year = 1984;
+// const age = current_year-birth_Year;
+// const job = "Software Engineer";
 
 // Template Literal
-let phil_template = `I'm ${first_name} ${last_name}, a ${age} year old ${job}.`
-console.log(phil_template)
+// let phil_template = `I'm ${first_name} ${last_name}, a ${age} year old ${job}.`
+// console.log(phil_template)
+
+//Logical Operators
+const hasDriversLicense = true;
+const hasGoodVision = true;
+const isTired = false;
+
+if (hasDriversLicense && hasGoodVision && !isTired){
+    console.log("Should drive");
+} else {
+    console.log("Should not drive.");
+}
 
 /*
 let js = "Amazing";
 let fund_1_math = 40 + 8 + 23 - 10;
 console.log(fund_1_math)
 console.log(first_name)
-strict comparison ===
-loose comparison ==
+
+
+Notes: 
+strict comparison: ===
+loose comparison: ==
+and: &&
+or: ||
+not: !
 */
